aa3d9375b85331f3be9e83be6c562f9d5ab3883e
Alexander Schulze <alexanderschulze@users.noreply.github.com>
2019-01-25 08:06:33 +0100

    Update urls.py

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -7,10 +7,10 @@ from . import views
 
 app_name = 'url_shortener'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<alias>[a-zA-Z0-9-_]+)$', views.redirect, name='alias'),
-    url(r'^(?P<alias>[a-zA-Z0-9-_]+)(?P<extra>/.*)$', views.redirect, name='alias'),
-    url(r'^(?P<alias>[a-zA-Z0-9-_]+)\+$', views.preview, name='preview'),
+    url(r'^new-short-url/$', views.index, name='index'),
+    url(r'^(?P<alias>[a-zA-Z0-9-_]*)$', views.redirect, name='alias'),
+    url(r'^(?P<alias>[a-zA-Z0-9-_]*)(?P<extra>/.*)$', views.redirect, name='alias'),
+    url(r'^(?P<alias>[a-zA-Z0-9-_]*)\+$', views.preview, name='preview'),
     url(r'^~analytics/$', views.analytics, name='analytics'),
 ]
 
